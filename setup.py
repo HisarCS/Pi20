3a38c3377d5d7df76fc5629499b19e2e43852de4
AndyEmre <55792307+AndyEmreHis@users.noreply.github.com>
2020-01-12 00:35:11 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
 from setuptools import setup
 import subprocess
 
-"""output = subprocess.check_output(['bash','-c', "sudo pip3 install opencv-contrib-python"])
-output = subprocess.check_output(['bash','-c', "sudo apt-get install libcblas-dev"])
-output = subprocess.check_output(['bash','-c', "sudo apt-get install libhdf5-dev"])
-output = subprocess.check_output(['bash','-c', "sudo apt-get install libhdf5-serial-dev"])
-output = subprocess.check_output(['bash','-c', "sudo apt-get install libatlas-base-dev"])
-output = subprocess.check_output(['bash','-c', "sudo apt-get install libjasper-dev "])
-output = subprocess.check_output(['bash','-c', "sudo apt-get install libqtgui4"])
-output = subprocess.check_output(['bash','-c', "sudo apt-get install libqt4-test"])"""
 
-with open("README.md", "r") as fh:
-      long_description = fh.read()
+#with open("README.md", "r") as fh:
+      #long_description = fh.read()
 
 
 print("                                                                                 ")
@@ -30,8 +22,8 @@ setup(
     author_email = "emre.kocak@hisarschool.k12.tr, rana.taki@hisarschool.k12.tr",
     description = "Library that makes use of sensors, motors, and servos in the PiWars Turkey robot kit by HisarCS",
     packages = ["Pi20"],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
     classifiers=["Development Status :: 4 - Beta"],
     install_requires=[
         'picamera',
