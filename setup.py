0bb45ed0824b63b1c96759697867515b8db22b81
AndyEmre <55792307+AndyEmreHis@users.noreply.github.com>
2020-01-12 10:55:13 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 import subprocess
 
 
-#with open("README.md", "r") as fh:
-      #long_description = fh.read()
+with open("README.md", "r") as fh:
+      long_description = fh.read()
 
 
 print("                                                                                 ")
@@ -22,8 +22,8 @@ setup(
     author_email = "emre.kocak@hisarschool.k12.tr, rana.taki@hisarschool.k12.tr",
     description = "Library that makes use of sensors, motors, and servos in the PiWars Turkey robot kit by HisarCS",
     packages = ["Pi20"],
-    #long_description=long_description,
-    #long_description_content_type="text/markdown",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=["Development Status :: 4 - Beta"],
     install_requires=[
         'picamera',
