a185ced2efee1455e2edba95d73e7784d17cc129
AndyEmre <55792307+AndyEmreHis@users.noreply.github.com>
2020-01-14 12:46:38 +0700

    Update __init__.py

--- Pi20/__init__.py
+++ Pi20/__init__.py
@@ -2,3 +2,4 @@ from .KumandaKontrol import Kumanda
 from .MotorKontrol import MotorKontrol
 from .ServoKontrol import ServoKontrol
 from .UltrasonikSensor import UltrasonikSensor
+from .PiKamera20 import HizlandirilmisPiKamera
