7d1c819bb2a5c5fe211d9af252f43ab350a1d33a
Andy Emre Kocak <55792307+AndyEmreHis@users.noreply.github.com>
2020-01-27 12:41:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 from setuptools import setup
 import subprocess
+import os
 
-
-with open("README.md", "r") as fh:
-      long_description = fh.read()  
-
+os.system("echo hello")
+os.system('sudo apt-get install libcblas-dev')
+os.system('sudo apt-get install libhdf5-dev')
+os.system('sudo apt-get install libhdf5-serial-dev')
+os.system('sudo apt-get install libqtwebkit4')
+os.system('sudo apt-get install libqt4-test')
+os.system('sudo apt-get install libatlas-base-dev')
+os.system('sudo apt-get install libjasper-dev')
 
 setup(
     name = "Pi20",
-    version = "0.1",
-    author = "Andy Emre Kocak, Rana Taki",
-    author_email = "emre.kocak@hisarschool.k12.tr, rana.taki@hisarschool.k12.tr",
+    version = "0.1.5",
+    author = "Andy Emre Kocak, Rana Taki, Yoel Kastro, Yasar Idikut",
+    author_email = "emre.kocak@hisarschool.k12.tr, rana.taki@hisarschool.k12.tr, sarp.kastro@hisarschool.k12.tr, yasar.idikut@hisarschool.k12.tr",
     description = "Library that makes use of sensors, motors, and servos in the PiWars Turkey robot kit by HisarCS",
     packages = ["Pi20"],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     classifiers=["Development Status :: 4 - Beta"],
     install_requires=[
         'picamera',
@@ -22,7 +25,7 @@ setup(
         'RPi.GPIO',
         'wiringpi',
         'numpy',
-        'opencv-contrib-python'
+        'opencv-contrib-python==3.4.3.18'
     ]
 
 )
